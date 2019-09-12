f359ea1d18232ee1c35fb0c55f6c6f60e5f19cfe
soli <liuwanli@taihe.com>
2019-09-12 17:27:20 +0800

    flutter 自绘制差不多了

--- flutter_module/lib/widget/TurnBox.dart
+++ flutter_module/lib/widget/TurnBox.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'custom_flow.dart';
 import 'custom_paint.dart';
 
 class TurnBox extends StatefulWidget {
   const TurnBox(
-      {Key key,
-      @required this.turns,
-      @required this.speed,
-      @required this.child})
-      : assert(speed > 0),
-        assert(child != null),
+      {Key key,  this.turns = 0, this.speed: 10, @required this.child})
+      : assert(child != null),
         super(key: key);
 
   final double turns;
@@ -58,6 +55,27 @@ class _TurnBoxState extends State<TurnBox> with SingleTickerProviderStateMixin {
   }
 }
 
+class FixedSizeLayoutDelegate extends SingleChildLayoutDelegate {
+  const FixedSizeLayoutDelegate({this.size});
+
+  final Size size;
+
+  @override
+  bool shouldRelayout(FixedSizeLayoutDelegate oldDelegate) {
+    return size != oldDelegate.size;
+  }
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    return size;
+  }
+
+  @override
+  BoxConstraints getConstraintsForChild(BoxConstraints constraints) {
+    return BoxConstraints.tight(size);
+  }
+}
+
 class TurnBoxRoute extends StatefulWidget {
   @override
   _TurnBoxRouteState createState() => new _TurnBoxRouteState();
@@ -69,42 +87,75 @@ class _TurnBoxRouteState extends State<TurnBoxRoute> {
   @override
   Widget build(BuildContext context) {
     debugPrint("_TurnBoxRouteState---build");
-    return ListView(
-      children: <Widget>[
-        CustomPaintDemo(),
-        TurnBox(
-          turns: _turns,
-          speed: 500,
-          child: Icon(
-            Icons.refresh,
-            size: 50,
+    MediaQueryData data = MediaQuery.of(context);
+    return ConstrainedBox(
+        constraints: BoxConstraints.expand(),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.centerLeft,
+                color: Colors.red.withOpacity(0.4),
+                constraints: BoxConstraints.tight(Size(data.size.width, 50)),
+                child: FlowMenu(),
+              ),
+              CustomPaintDemo(),
+              TurnBox(
+                turns: _turns,
+                speed: 500,
+                child: Icon(
+                  Icons.refresh,
+                  size: 50,
+                ),
+              ),
+              TurnBox(
+                turns: _turns,
+                speed: 1000,
+                child: Icon(
+                  Icons.refresh,
+                  size: 150.0,
+                ),
+              ),
+              RaisedButton(
+                child: Text("顺时针旋转1/5圈"),
+                onPressed: () {
+                  setState(() {
+                    _turns += .2;
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text("逆时针旋转1/5圈"),
+                onPressed: () {
+                  setState(() {
+                    _turns -= .2;
+                  });
+                },
+              ),
+              Container(
+                color: Colors.blue,
+                padding: const EdgeInsets.all(5.0),
+                child: CustomSingleChildLayout(
+                  delegate: FixedSizeLayoutDelegate(size: Size(100.0, 50.0)),
+                  child: Container(
+                    color: Colors.red,
+                    width: 100.0,
+                    height: 300.0,
+                  ),
+                ),
+              ),
+              GradientCircularProgressRoute(),
+//              GradientCircularProgressIndicator(
+//                // No gradient
+//                colors: [Colors.blue, Colors.blue],
+//                radius: 40.0,
+//                strokeCapRound: true,
+//                strokeWidth: 8.0,
+//                value: .5,
+//              ),
+            ],
           ),
-        ),
-        TurnBox(
-          turns: _turns,
-          speed: 1000,
-          child: Icon(
-            Icons.refresh,
-            size: 150.0,
-          ),
-        ),
-        RaisedButton(
-          child: Text("顺时针旋转1/5圈"),
-          onPressed: () {
-            setState(() {
-              _turns += .2;
-            });
-          },
-        ),
-        RaisedButton(
-          child: Text("逆时针旋转1/5圈"),
-          onPressed: () {
-            setState(() {
-              _turns -= .2;
-            });
-          },
-        )
-      ],
-    );
+        ));
   }
 }
--- flutter_module/lib/widget/custom_paint.dart
+++ flutter_module/lib/widget/custom_paint.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_module/widget/TurnBox.dart';
 
 class CustomPaintDemo extends StatelessWidget {
   @override
@@ -8,24 +9,31 @@ class CustomPaintDemo extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Center(
-        child: CustomPaint(
-          size: Size(300, 300),
-          painter: MyPainter(),
+        child: RepaintBoundary(
+            child: CustomPaint(
+          size: Size(300, 300), //有child的时候这个不管用
+          painter: _ChessPainter(),
 //          foregroundPainter: MyPainter(),
-          child: Container(
-            alignment: Alignment.topCenter,
-//            color: Colors.green,
-            width: 300,
-            height: 300,
-            child: RaisedButton(child: Text("我是谁"),onPressed: (){},),
+          child: RepaintBoundary(
+            child: Container(
+              alignment: Alignment.topCenter,
+//            color: Colors.red,
+              width: 300,
+              height: 300,
+              child: Container(
+                color: Colors.green.withOpacity(.5),
+                width: 300,
+                height: 40,
+              ),
+            ),
           ),
-        ),
+        )),
       ),
     );
   }
 }
 
-class MyPainter extends CustomPainter {
+class _ChessPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     double eWidth = size.width / 15;
@@ -69,6 +77,345 @@ class MyPainter extends CustomPainter {
         Offset(size.width / 2 + eWidth / 2, size.height / 2 + eHeight / 2),
         min(eWidth / 2, eHeight / 2) - 2,
         paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class GradientCircularProgressRoute extends StatefulWidget {
+  @override
+  GradientCircularProgressRouteState createState() {
+    return GradientCircularProgressRouteState();
+  }
+}
+
+class GradientCircularProgressRouteState
+    extends State<GradientCircularProgressRoute> with TickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        new AnimationController(vsync: this, duration: Duration(seconds: 3));
+    bool isForward = true;
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.forward) {
+        isForward = true;
+      } else if (status == AnimationStatus.completed ||
+          status == AnimationStatus.dismissed) {
+        if (isForward) {
+          _animationController.reverse();
+        } else {
+          _animationController.forward();
+        }
+      } else if (status == AnimationStatus.reverse) {
+        isForward = false;
+      }
+    });
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        AnimatedBuilder(
+          animation: _animationController,
+          builder: (BuildContext context, Widget child) {
+            return Column(
+              children: <Widget>[
+                Wrap(
+                  spacing: 10.0,
+                  runSpacing: 16.0,
+                  children: <Widget>[
+                    GradientCircularProgressIndicator(
+                      // No gradient
+                      colors: [Colors.blue, Colors.blue],
+                      radius: 30.0,
+                      strokeCapRound: true,
+                      strokeWidth: 5.0,
+                      value: _animationController.value,
+                    ),
+                    GradientCircularProgressIndicator(
+                      colors: [Colors.red, Colors.orange],
+                      radius: 30.0,
+                      strokeWidth: 3.0,
+                      value: _animationController.value,
+                    ),
+                    GradientCircularProgressIndicator(
+                      colors: [Colors.red, Colors.orange, Colors.red],
+                      radius: 30.0,
+                      strokeWidth: 5.0,
+                      value: _animationController.value,
+                    ),
+                    GradientCircularProgressIndicator(
+                      colors: [Colors.teal, Colors.cyan],
+                      radius: 30.0,
+                      strokeWidth: 5.0,
+                      strokeCapRound: true,
+                      value: CurvedAnimation(
+                              parent: _animationController,
+                              curve: Curves.bounceIn)
+                          .value,
+                    ),
+                    TurnBox(
+                      turns: 1 / 8,
+                      child: GradientCircularProgressIndicator(
+                          colors: [Colors.red, Colors.orange, Colors.red],
+                          radius: 30.0,
+                          strokeWidth: 5.0,
+                          strokeCapRound: true,
+                          backgroundColor: Colors.red[50],
+                          totalAngle: 1.5 * pi,
+                          value: CurvedAnimation(
+                                  parent: _animationController,
+                                  curve: Curves.ease)
+                              .value),
+                    ),
+                    RotatedBox(
+                      quarterTurns: 1,
+                      child: GradientCircularProgressIndicator(
+                          colors: [Colors.blue[700], Colors.blue[200]],
+                          radius: 30.0,
+                          strokeWidth: 3.0,
+                          strokeCapRound: true,
+                          backgroundColor: Colors.transparent,
+                          value: _animationController.value),
+                    ),
+                    GradientCircularProgressIndicator(
+                      colors: [
+                        Colors.red,
+                        Colors.amber,
+                        Colors.cyan,
+                        Colors.green[200],
+                        Colors.blue,
+                        Colors.red
+                      ],
+                      radius: 30.0,
+                      strokeWidth: 5.0,
+                      strokeCapRound: true,
+                      value: _animationController.value,
+                    ),
+                  ],
+                ),
+                GradientCircularProgressIndicator(
+                  colors: [Colors.blue[700], Colors.blue[200]],
+                  radius: 50.0,
+                  strokeWidth: 10.0,
+                  value: _animationController.value,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: GradientCircularProgressIndicator(
+                    colors: [Colors.blue[700], Colors.blue[300]],
+                    radius: 50.0,
+                    strokeWidth: 10.0,
+                    value: _animationController.value,
+                    strokeCapRound: true,
+                  ),
+                ),
+                //剪裁半圆
+                Align(
+                    heightFactor: 0.5,
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: TurnBox(
+                        turns: .75,
+                        child: GradientCircularProgressIndicator(
+                          colors: [Colors.teal, Colors.cyan[500]],
+                          radius: 50.0,
+                          strokeWidth: 10.0,
+                          value: _animationController.value,
+                          totalAngle: pi,
+                          strokeCapRound: true,
+                        ),
+                      ),
+                    )),
+                //注意和上面的区别
+                TurnBox(
+                  turns: .75,
+                  child: GradientCircularProgressIndicator(
+                    colors: [Colors.teal, Colors.cyan[500]],
+                    radius: 50.0,
+                    strokeWidth: 10.0,
+                    value: _animationController.value,
+//                    totalAngle: pi,
+                    strokeCapRound: true,
+                  ),
+                ),
+                Container(
+                  color: Colors.grey.withOpacity(0.5),
+                  height: 52.0,
+                  width: 100.0,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      Positioned(
+//                        height: 200.0,
+                        top: .0,
+                        child: TurnBox(
+                          turns: 0.75,
+                          child: GradientCircularProgressIndicator(
+                            colors: [Colors.teal, Colors.cyan[500]],
+                            radius: 50.0,
+                            strokeWidth: 8.0,
+                            value: _animationController.value,
+                            totalAngle: pi,
+                            strokeCapRound: true,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Text(
+                          "${(_animationController.value * 100).toInt()}%",
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class GradientCircularProgressIndicator extends StatelessWidget {
+  GradientCircularProgressIndicator(
+      {this.strokeWidth = 2.0,
+      @required this.radius,
+      @required this.colors,
+      this.stops,
+      this.strokeCapRound = false,
+      this.backgroundColor = const Color(0xFFEEEEEE),
+      this.totalAngle = 2 * pi,
+      this.value});
+
+  ///粗细
+  final double strokeWidth;
+
+  /// 圆的半径
+  final double radius;
+
+  ///两端是否为圆角
+  final bool strokeCapRound;
+
+  /// 当前进度，取值范围 [0.0-1.0]
+  final double value;
+
+  /// 进度条背景色
+  final Color backgroundColor;
+
+  /// 进度条的总弧度，2*PI为整圆，小于2*PI则不是整圆
+  final double totalAngle;
+
+  /// 渐变色数组
+  final List<Color> colors;
+
+  /// 渐变色的终止点，对应colors属性
+  final List<double> stops;
+
+  @override
+  Widget build(BuildContext context) {
+    double _offset = .0;
+    // 如果两端为圆角，则需要对起始位置进行调整，否则圆角部分会偏离起始位置
+    // 下面调整的角度的计算公式是通过数学几何知识得出，读者有兴趣可以研究一下为什么是这样
+    if (strokeCapRound) {
+      _offset = asin(strokeWidth / (radius * 2 - strokeWidth));
+    }
+    var _colors = colors;
+    if (_colors == null) {
+      Color color = Theme.of(context).accentColor;
+      _colors = [color, color];
+    }
+    return Transform.rotate(
+      angle: -pi / 2.0 - _offset,
+      child: CustomPaint(
+          size: Size.fromRadius(radius),
+          painter: _GradientCircularProgressPainter(
+            strokeWidth: strokeWidth,
+            strokeCapRound: strokeCapRound,
+            backgroundColor: backgroundColor,
+            value: value,
+            total: totalAngle,
+            radius: radius,
+            colors: _colors,
+          )),
+    );
+  }
+}
+
+class _GradientCircularProgressPainter extends CustomPainter {
+  const _GradientCircularProgressPainter(
+      {this.strokeWidth: 10.0,
+      this.strokeCapRound = false,
+      this.backgroundColor = const Color(0xFFEEEEEE),
+      this.radius,
+      this.total = 2 * pi,
+      @required this.colors,
+      this.stops,
+      this.value});
+
+  final double strokeWidth;
+  final bool strokeCapRound;
+  final double value;
+  final Color backgroundColor;
+  final List<Color> colors;
+  final double total;
+  final double radius;
+  final List<double> stops;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (radius != null) size = Size.fromRadius(radius);
+
+    double _offset = strokeWidth / 2.0;
+    double _value = value ?? .0;
+    _value = _value.clamp(.0, 1) * total;
+    double _start = .0;
+
+    if (strokeCapRound) _start = asin(strokeWidth / (size.width - strokeWidth));
+
+    Rect rect = Offset(_offset, _offset) &
+        Size(size.width - strokeWidth, size.height - strokeWidth);
+
+    var paint = Paint()
+      ..strokeCap = strokeCapRound ? StrokeCap.round : StrokeCap.butt
+      ..style = PaintingStyle.stroke
+      ..isAntiAlias = true
+      ..strokeWidth = strokeWidth;
+
+    // 先画背景
+    if (backgroundColor != Colors.transparent) {
+      paint.color = backgroundColor;
+      canvas.drawArc(rect, _start, total, false, paint);
+    }
+
+    // 再画前景，应用渐变
+    if (_value > 0) {
+      paint.shader = SweepGradient(
+              colors: colors, startAngle: 0.0, endAngle: _value, stops: stops)
+          .createShader(rect);
+
+      canvas.drawArc(rect, _start, _value, false, paint);
+    }
   }
 
   @override
